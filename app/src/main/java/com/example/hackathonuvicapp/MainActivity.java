8008ca2480c243f9cdb12537dcafdcb8a095d27a
Kaiden <kaidenrivett@uvic.ca>
2021-01-09 15:11:13 -0800

    adding uvic colours

--- app/src/main/java/com/example/hackathonuvicapp/MainActivity.java
+++ app/src/main/java/com/example/hackathonuvicapp/MainActivity.java
@@ -2,13 +2,8 @@ package com.example.hackathonuvicapp;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_page);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
