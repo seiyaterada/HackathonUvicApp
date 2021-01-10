c1c7429899b6b5fff1353afe6a30f72a4209d4bc
Kaiden <kaidenrivett@uvic.ca>
2021-01-09 17:17:41 -0800

    more google button implementation

--- app/src/main/java/com/example/hackathonuvicapp/LoginActivity.java
+++ app/src/main/java/com/example/hackathonuvicapp/LoginActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,7 +17,36 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginPage);
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestEmail()
+//                .build();
+//        GoogleSignInClient = GoogleSignIn.getClient(this, gso);
+//
+//        findViewById(R.id.sign_in_button).setOnClickListener(this)
     }
+
+//    public void updateUI(FirebaseUser account){
+//
+//        if(account != null){
+//            Toast.makeText(this,"U Signed In successfully",Toast.LENGTH_LONG).show();
+//            startActivity(new Intent(this,AnotherActivity.class));
+//
+//        }else {
+//            Toast.makeText(this,"U Didnt signed in",Toast.LENGTH_LONG).show();
+//        }
+//
+//    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Toast.makeText(getApplicationContext(),"Now onStart() calls", Toast.LENGTH_LONG).show(); //onStart Called
+//        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+//        updateUI(account);
+    }
+
+
 
     //     Google sign in stuff below, not very relevant right now
 
