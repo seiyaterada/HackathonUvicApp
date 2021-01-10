d8dc4a47927dfa6f39fd1be86bfa8acfc00057f2
Seiya Terada <seiyaterada@uvic.ca>
2021-01-09 16:42:28 -0800

    changed activity names

--- app/src/main/java/com/example/hackathonuvicapp/MainActivity.java
+++ app/src/main/java/com/example/hackathonuvicapp/MainActivity.java
@@ -9,36 +9,24 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.cover_page);
+
+        //configureLoginButton();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+//    private void configureLoginButton(){
+//        Button loginPageButton
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 
     // Google sign in stuff below
 
