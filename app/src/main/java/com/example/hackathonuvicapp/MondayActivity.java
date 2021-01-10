733f1e8189122fbd4baf868f439becff8cd11edd
Seiya Terada <seiyaterada@uvic.ca>
2021-01-10 02:11:07 -0800

    Update on addClass Dialog

--- app/src/main/java/com/example/hackathonuvicapp/MondayActivity.java
+++ app/src/main/java/com/example/hackathonuvicapp/MondayActivity.java
@@ -4,14 +4,18 @@ import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
     public class MondayActivity extends AppCompatActivity {
+
+        String m_Text;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +24,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
             ConfigurePrev();
             ConfigureNext();
-            //addClass();
+            addClass();
         }
 
-        private void ConfigurePrev(){
+        private void ConfigurePrev() {
             ImageButton prevButton = (ImageButton) findViewById(R.id.btnMonFri);
-            prevButton.setOnClickListener(new View.OnClickListener(){
+            prevButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     startActivity(new Intent(MondayActivity.this, LoginActivity.class));
@@ -34,9 +38,9 @@ import androidx.appcompat.app.AppCompatActivity;
             });
         }
 
-        private void ConfigureNext(){
+        private void ConfigureNext() {
             ImageButton nextButton = (ImageButton) findViewById(R.id.btnMonTue);
-           nextButton.setOnClickListener(new View.OnClickListener(){
+            nextButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     startActivity(new Intent(MondayActivity.this, TuesdayActivity.class));
@@ -46,22 +50,38 @@ import androidx.appcompat.app.AppCompatActivity;
         }
 
 
-//          alertdialog beginning
-//        private void addClass() {
-//            Button addClassButton = (Button) findViewById(R.id.btnNew);
-//            addClassButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    AlertDialog.Builder builder = new AlertDialog.Builder(MondayActivity.this);
-//                    builder.setMessage("This is a test")
-//                            .setPositiveButton("Testing", new DialogInterface.OnClickListener() {
-//                                @Override
-//                                public void onClick(DialogInterface dialog, int which) {
-//                                    btnClass.setText("Here is some stuff");
-//                                }
-//                            });
-//                    startActivity(new Intent(MondayActivity.this, MondayActivityOneCourse.class));
-//                }
-//            });
-//        }
-}
+        //          alertdialog beginning
+        private void addClass() {
+            Button addClassButton = findViewById(R.id.btnNew);
+            addClassButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MondayActivity.this);
+                    builder.setTitle("Add Course");
+
+                    // Set up the input
+                    final EditText input = new EditText(MondayActivity.this);
+                    // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                    input.setInputType(InputType.TYPE_CLASS_TEXT);
+                    builder.setView(input);
+
+                    // Set up the buttons
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            m_Text = input.getText().toString();
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+
+                    builder.show();
+                }
+
+            });
+        }
+    }
