719ed185e392754eb5d83c70ec880d1048c48b21
Seiya Terada <seiyaterada@uvic.ca>
2021-01-09 16:51:07 -0800

    login button works

--- app/src/main/java/com/example/hackathonuvicapp/MainActivity.java
+++ app/src/main/java/com/example/hackathonuvicapp/MainActivity.java
@@ -18,12 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cover_page);
 
-        //configureLoginButton();
+        configureLoginButton();
     }
 
-//    private void configureLoginButton(){
-//        Button loginPageButton
-//    }
+    private void configureLoginButton(){
+        Button loginPageButton = (Button) findViewById(R.id.button_sign_in);
+        loginPageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+
+        });
+    }
 
 
 
